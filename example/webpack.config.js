c754ced73b96de042e43fd88715aae9aedf60730
ZYSzys <17367077526@163.com>
2019-02-16 20:47:04 +0800

    prettier

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -12,7 +12,6 @@ const port = process.env.PORT || 3001;
  * For more information, see: http://webpack.github.io/docs/configuration.html
  */
 module.exports = {
-
   // Efficiently evaluate modules with source maps
   devtool: 'eval',
 
@@ -45,19 +44,20 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel',
       },
-      { test: /\.css$/, exclude: /\.useable\.css$/, loader: "style!css" },
-      { test: /\.useable\.css$/, loader: "style/useable!css" },
-      { 
-        test: /\.css$/, 
-        loader: "style-loader!css-loader" 
+      { test: /\.css$/, exclude: /\.useable\.css$/, loader: 'style!css' },
+      { test: /\.useable\.css$/, loader: 'style/useable!css' },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
       },
       {
         test: /\.scss$/,
-        loaders: ["style", "css", "sass"]
+        loaders: ['style', 'css', 'sass'],
       },
-      { test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader : 'file-loader' 
-      }
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file-loader',
+      },
     ],
   },
 
