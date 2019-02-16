2a9896bf6fade719f0c02842d638fd962d9299ee
ZYSzys <17367077526@163.com>
2019-02-16 22:21:10 +0800

    example: tweak

--- example/main.js
+++ example/main.js
@@ -5,25 +5,24 @@ import Sharing from '../src';
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Sharing
-          sites={[
-            'qzone',
-            'weibo',
-            'google',
-            'twitter',
-            'qq',
-            'tencent',
-            'wechat',
-            'douban',
-            'linkedin',
-            'facebook',
-          ]}
-          url="ttps://github.com/ZYSzys/react-sharing"
-          title="react-sharing"
-          description="一键分享到微博、QQ空间、QQ好友、微信、腾讯微博、豆瓣、Facebook、Twitter、Linkedin、Google+ 的 react 组件"
-        />
-      </div>
+      <Sharing
+        sites={[
+          'qzone',
+          'weibo',
+          'google',
+          'twitter',
+          'qq',
+          'tencent',
+          'wechat',
+          'douban',
+          'linkedin',
+          'facebook',
+        ]}
+        summary="fuck"
+        url="https://github.com/ZYSzys/react-sharing"
+        title="react-sharing"
+        description="一键分享到微博、QQ空间、QQ好友、微信、腾讯微博、豆瓣、Facebook、Twitter、Linkedin、Google+ 的 react 组件"
+      />
     );
   }
 }
