11266a364370abd56206b33be4778406edf1e773
ZYSzys <17367077526@163.com>
2019-02-17 11:29:36 +0800

    Updates

---
+++ precache-manifest.5f3c1fb033caa1148c58bd7643837160.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a960df27460cc05d86b",
+    "url": "/react-sharing/static/css/main.357f7d17.chunk.css"
+  },
+  {
+    "revision": "8a960df27460cc05d86b",
+    "url": "/react-sharing/static/js/main.8a960df2.chunk.js"
+  },
+  {
+    "revision": "cad76ac03f6630294154",
+    "url": "/react-sharing/static/js/runtime~main.cad76ac0.js"
+  },
+  {
+    "revision": "d9874a148292d4c933f4",
+    "url": "/react-sharing/static/js/2.d9874a14.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-sharing/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "392e171e22851a50d7e306fb8a6c15f9",
+    "url": "/react-sharing/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b8a7e7a9e5a3036d1addeb0e9f7ee44f.js"
+  "/react-sharing/precache-manifest.5f3c1fb033caa1148c58bd7643837160.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-sharing/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
